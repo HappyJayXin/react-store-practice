d741a92fe2f739538f3edd5b0e3208fb4ee776c4
Jay <4a490063@stust.edu.tw>
2020-02-06 16:22:01 +0800

    Feat: 11- 函数式组件

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,30 @@
-import React, { Component } from 'react';
-import { type } from 'os';
+import React, {FunctionComponent} from 'react';
 
 type HeaderState = {
   nickname: string;
 };
 
-export default class Header extends Component<HeaderState> {
-  // constructor(props: HeaderState) {
-  //   super(props);
-  // }
+const Header: FunctionComponent<HeaderState> = ({nickname}) => (
+  <header className="header">
+    <section className="grid">
+      <div className="start">
+        <a href="/">Home</a>
+      </div>
+      <div className="end">
+        {nickname ? (
+          <span className="nickname">
+            <i className="fas fa-user"></i>
+            {nickname}
+          </span>
+        ) : (
+          <>
+            <a href="/">Login</a>
+            <a href="/">Register</a>
+          </>
+        )}
+      </div>
+    </section>
+  </header>
+);
 
-  renderLink() {
-    const { nickname } = this.props;
-    if (nickname) {
-      return (
-        <span className="nickname">
-          <i className="fas fa-user"></i>
-          {this.props.nickname}
-        </span>
-      );
-    } else {
-      return (
-        <>
-          <a href="/">Login</a>
-          <a href="/">Register</a>
-        </>
-      );
-    }
-  }
-
-  render() {
-    return (
-      <header className="header">
-        <section className="grid">
-          <div className="start">
-            <a href="/">Home</a>
-          </div>
-          <div className="end">{this.renderLink()}</div>
-        </section>
-      </header>
-    );
-  }
-}
+export default Header;
