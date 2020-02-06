f4c76f8d0d66e64fd920f5f97fa1374be9d2b45b
Jay <4a490063@stust.edu.tw>
2020-02-06 16:08:15 +0800

    Feat: 10-props 组件传参

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="main">
-        <Header />
+        <Header nickname="Admin" />
         <Products />
       </div>
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import { type } from 'os';
 
-export default class Header extends Component {
+type HeaderState = {
+  nickname: string;
+};
+
+export default class Header extends Component<HeaderState> {
+  // constructor(props: HeaderState) {
+  //   super(props);
+  // }
+
+  renderLink() {
+    const { nickname } = this.props;
+    if (nickname) {
+      return (
+        <span className="nickname">
+          <i className="fas fa-user"></i>
+          {this.props.nickname}
+        </span>
+      );
+    } else {
+      return (
+        <>
+          <a href="/">Login</a>
+          <a href="/">Register</a>
+        </>
+      );
+    }
+  }
+
   render() {
     return (
       <header className="header">
@@ -8,10 +36,7 @@ export default class Header extends Component {
           <div className="start">
             <a href="/">Home</a>
           </div>
-          <div className="end">
-            <a href="/">Login</a>
-            <a href="/">Register</a>
-          </div>
+          <div className="end">{this.renderLink()}</div>
         </section>
       </header>
     );
