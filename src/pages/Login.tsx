6c4a03285320a88751f205767e2a66c46e05f474
Jay <4a490063@stust.edu.tw>
2020-02-07 21:01:19 +0800

    Feat: 16-受控组件.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps } from 'react-router-dom';
 
-export default class Login extends Component<RouteComponentProps> {
-  emailRef = React.createRef<HTMLInputElement>();
-  passwordRef = React.createRef<HTMLInputElement>()
-  
+export default class Login extends Component<RouteComponentProps, LoginState> {
+  constructor(props: RouteComponentProps) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
   handleSubmit = (event: React.FormEvent<HTMLInputElement>) => {
     event.preventDefault();
-    const formData = {
-      email: this.emailRef.current!.value,
-      password: this.passwordRef.current!.value
-    }
-    console.log(formData);
-    // this.props.history.push('/'); 
+    console.log(this.state);
+    // this.props.history.push('/');
+  };
+
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    } as Pick<LoginState, keyof LoginState>)
   }
 
   render() {
@@ -22,13 +30,13 @@ export default class Login extends Component<RouteComponentProps> {
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Email" ref={this.emailRef} />
+              <input className="input" type="text" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} />
             </div>
           </div>
           <div className="field">
             <label>Password</label>
             <div className="control">
-              <input className="input" type="text" placeholder="Password" ref={this.passwordRef} />
+              <input className="input" type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} />
             </div>
           </div>
           <div className="control">
@@ -38,4 +46,9 @@ export default class Login extends Component<RouteComponentProps> {
       </div>
     );
   }
+}
+
+interface LoginState {
+  email: string;
+  password: string
 }
