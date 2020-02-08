c45534bc026f3e21a5c8aa227a5d2af3c80dee99
Jay <4a490063@stust.edu.tw>
2020-02-08 10:53:50 +0800

    Feat: 18-商品组件布局.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,7 +4,22 @@ export default class Product extends Component {
   render() {
     return (
       <div className="product">
-        Product
+        <div className="p-content">
+          <div className="img-wrapper">
+            <figure className="image is4by3">
+              <img src="images/1.jpg" alt="shoes"/>
+            </figure>
+            <p className="p-tags">25 colors</p>
+            <p className="p-name">New Shoes</p>
+          </div>
+        </div>
+        <div className="p-footer">
+          <p className="price">$5380</p>
+          <button className="add-cart">
+            <i className="fas fa-shopping-cart"></i>
+            <i className="fas fa-exclamation"></i>
+          </button>
+        </div>
       </div>
     )
   }
